331819e9a493f70546c024ad162638d517a59d90
Chase Southard <chase.southard@gmail.com>
2014-04-18 00:21:30 -0400

    not functinoal but getting there

--- spec/whiff/arp_scan_spec.rb
+++ spec/whiff/arp_scan_spec.rb
@@ -1,7 +1,24 @@
 require "spec_helper"
 
+class BackTicks
+  attr_accessor :ip_address, :interface
+
+  def run
+    `arp-scan -l -s #{ip_address} -I #{interface} -q`
+  end
+end
+
 module Whiff::ArpScan
-  describe "it fetches MAC addresses through arp-scan" do
-    pending("not sure how to stub this out just yet")
+  describe "fetching MAC addresses through arp-scan" do
+    subject { BackTicks.new }
+
+    let(:ip_address) { '10.0.1.1' }
+    let(:interface) { 'en1' }
+
+    it "should execute arp scan with params" do
+      pending "not sure how to mock out backticks"
+      cmd = "arp-scan -l -s #{ip_address} -I #{interface} -q"
+      subject.should_receive(:`).with(cmd).and_return(["00:13:21:c1:28:9a"])
+    end
   end
 end
